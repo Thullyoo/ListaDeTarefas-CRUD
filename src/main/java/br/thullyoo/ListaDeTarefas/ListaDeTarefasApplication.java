5841ea3657e7d2857f884839e8a8464bc4cc0500
Thullyoo <thullyofilho@gmail.com>
2024-04-02 10:00:48 -0300

    feat: adicionando Swagger para documentar e banco de dados H2

--- src/main/java/br/thullyoo/ListaDeTarefas/ListaDeTarefasApplication.java
+++ src/main/java/br/thullyoo/ListaDeTarefas/ListaDeTarefasApplication.java
@@ -1,9 +1,12 @@
 package br.thullyoo.ListaDeTarefas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Lista de Tarefas API", version = "1.0", description = "API para um sistema de criação de lista de tarefas"))
 public class ListaDeTarefasApplication {
 
 	public static void main(String[] args) {
